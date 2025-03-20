f943f41796ac6c751a8d5fc1a98debaddb36e43f
FahiraIlham <fahirailham4@gmail.com>
2025-03-20 15:12:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import react from 'react';
+import './App.css';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import BookingForm from './components/BookingForm';
+import Menu from './components/Menu';
+
+
+function App() {
+  return (
+    <>
+    <Nav />
+    <Main />
+    <Menu />
+  </>
+);
+}
+
+export default App;
---
+++ recipes.js
@@ -0,0 +1,30 @@
+const recipes = [
+      {
+        id: 1,
+        title: "Greek Salad" ,
+        price: 12.99,
+        image: "https://cupfulofkale.com/wp-content/uploads/2023/05/Vegan-Greek-Salad-735x1103.jpg.webp" ,
+     description: "The famous greek salad of crispy lettuce, peppers, olives and our chicago style feta cheese, garnished with crunchy garlic and rosemary croutons" ,
+      },
+      {
+        id: 2,
+        title: "Bruschetta",
+        price: 5.99,
+        image: "https://www.recipetineats.com/tachyon/2018/12/Bruschetta_2a.jpg?resize=900%2C1125&zoom=0.72" ,
+        description:
+        "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil." ,
+      },
+      {
+        id: 3,
+        title: "Lemon Dessert",
+        price: 4.78,
+        image:"https://www.recipetineats.com/tachyon/2021/06/French-Lemon-Tart_5-main.jpg?resize=1200%2C1680&zoom=0.54" ,
+        description:
+         "This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined." ,
+     },
+
+];
+
+{/* "https://i.bb.co/317GqZv/Bruschetta.png" */}
+{/* "https://i.bb.co/68c9bLv/greek-salad.jpg" */}
+{/* "https://i.bb.co/sm6MfcH/desert.jpg" */}
